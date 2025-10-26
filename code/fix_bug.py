6eff2f73f511f3e70451a7f4c11adebeee608838
Shiva vuppala07 <shivavuppala2007@gmail.com>
2025-10-26 12:10:40 +0530

    Delete code directory

--- code/fix_bug.py
+++
@@ -1,8 +0,0 @@
-def fix_bug_code(user_code):
-    # Example: check for common Python errors
-    if "print" not in user_code:
-        return user_code + "\n# Suggestion: Maybe add a print statement"
-    elif "==" in user_code and "if" not in user_code:
-        return user_code + "\n# Warning: comparison found outside if"
-    else:
-        return user_code + "\n# No obvious issues"
